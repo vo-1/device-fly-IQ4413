700faee9bb8cc03e6d73ec04ecd28e47d94e55a4
vo-1 <vo-1@yandex.ru>
2016-06-22 14:03:38 +0300

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/micromax/AQ5001/AndroidBoardVendor.mk
+-include vendor/fly/IQ4413/AndroidBoardVendor.mk
