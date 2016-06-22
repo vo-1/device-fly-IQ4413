2c5a9ece60c815135778b6e6bfc5467816dcac8c
vo-1 <vo-1@yandex.ru>
2016-06-22 14:23:16 +0300

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 cd ../../../..
 cd external/tinycompress
-git apply -v ../../device/micromax/AQ5001/patches/external_tinycompress/external_tinycompress.patch
+git apply -v ../../device/fly/IQ4413/patches/external_tinycompress/external_tinycompress.patch
 cd ../..
 cd frameworks/av
-git apply -v ../../device/micromax/AQ5001/patches/frameworks_av/frameworks_av.patch
+git apply -v ../../device/fly/IQ4413/patches/frameworks_av/frameworks_av.patch
 cd ../..
 cd frameworks/base
-git apply -v ../../device/micromax/AQ5001/patches/frameworks_base/frameworks_base.patch
+git apply -v ../../device/fly/IQ4413/patches/frameworks_base/frameworks_base.patch
 cd ../..
 cd frameworks/opt/telephony
-git apply -v ../../../device/micromax/AQ5001/patches/frameworks_opt_telephony/frameworks_opt_telephony.patch
+git apply -v ../../../device/fly/IQ4413/patches/frameworks_opt_telephony/frameworks_opt_telephony.patch
 cd ../../..
 cd hardware/libhardware
-git apply -v ../../device/micromax/AQ5001/patches/hardware_libhardware/hardware_libhardware.patch
+git apply -v ../../device/fly/IQ4413/patches/hardware_libhardware/hardware_libhardware.patch
 cd ../..
 cd packages/services/Telecomm
-git apply -v ../../../device/micromax/AQ5001/patches/packages_services_Telecomm/services_Telecomm.patch
+git apply -v ../../../device/fly/IQ4413/patches/packages_services_Telecomm/services_Telecomm.patch
 cd ../../..
 cd packages/services/Telephony
-git apply -v ../../../device/micromax/AQ5001/patches/packages_services_Telephony/services_Telephony.patch
+git apply -v ../../../device/fly/IQ4413/patches/packages_services_Telephony/services_Telephony.patch
 cd ../../..
 cd system/core
-git apply -v ../../device/micromax/AQ5001/patches/system_core/system_core.patch
+git apply -v ../../device/fly/IQ4413/patches/system_core/system_core.patch
 cd ../..
 cd system/netd
-git apply -v ../../device/micromax/AQ5001/patches/system_netd/system_netd.patch
+git apply -v ../../device/fly/IQ4413/patches/system_netd/system_netd.patch
 cd ../..
 echo Patches Applied Successfully!
